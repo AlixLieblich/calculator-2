9c57c09756ba8743970338346e9e18073440dd22
Emily Willis <emilyw.nsc@gmail.com>
2021-02-25 20:38:56 +0000

    Made code more user input friendly

--- calculator.py
+++ calculator.py
@@ -6,25 +6,43 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 while True:
     user_input = input("Enter a calculation >  ")
     tokens = user_input.split(' ')
+    operator = tokens[0]
     if tokens[0] == 'q':
         print("Goodbye!")
         break
-    elif tokens[0] == '+':
-        print(add(float(tokens[1]), float(tokens[2])))
-    elif tokens[0] == '-':
-        print(subtract(float(tokens[1]), float(tokens[2])))    
-    elif tokens[0] == '*':
-        print(multiply(float(tokens[1]), float(tokens[2])))
-    elif tokens[0] == '/':
-        print(divide(float(tokens[1]), float(tokens[2])))
-    elif tokens[0] == 'square':
-        print(square(float(tokens[1])))
-    elif tokens[0] == 'power':
-        print(power(float(tokens[1]), float(tokens[2])))
-    elif tokens[0] == 'cube':
-        print(cube(float(tokens[1])))
-    elif tokens[0] == 'mod':
-        print(mod(float(tokens[1]), float(tokens[2])))
+    if len(tokens) < 2:
+        print("Not enough inputs.")
+    if len(tokens) == 2 or len(tokens) == 3:
+        num1 = float(tokens[1])
+    if len(tokens) == 3:
+        num2 = float(tokens[2])
+    if len(tokens) > 3:
+        print("Too many inputs.")
+
+
+
+    if operator == '+':
+        print(add(num1, num2))
+    elif operator == '-':
+        print(subtract(num1, num2))    
+    elif operator == '*':
+        print(multiply(num1, num2))
+    elif operator == '/':
+        print(divide(num1, num2))
+    elif operator == 'square':
+        if len(tokens) > 2:
+            print("Too many inputs.")
+        else:
+            print(square(num1))
+    elif operator == 'power':
+        print(power(num1, num2))
+    elif operator == 'cube':
+        if len(tokens) > 2:
+            print("Too many inputs.")
+        else:
+            print(cube(num1))
+    elif operator == 'mod':
+        print(mod(num1, num2))
     else:
         print("""
         Allowed operators are: +, -, *, /, square, power, cube, mod. 
